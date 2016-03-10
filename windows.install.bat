a4cb92fb359a2406b2b111706f21f16d0b542d85
f4exb <f4exb06@gmail.com>
2016-03-10 04:56:29 +0100

    Windows build: added a batch command file for install

---
+++ windows.install.bat
@@ -0,0 +1,17 @@
+copy app\%1\sdrangel.exe %2
+copy sdrbase\%1\sdrbase.dll %2
+copy lz4\%1\lz4.dll %2
+mkdir %2\plugins
+mkdir %2\plugins\channel
+mkdir %2\plugins\samplesource
+copy plugins\channel\chanalyzer\%1\chanalyzer.dll %2\plugins\channel
+copy plugins\channel\demodam\%1\demodam.dll %2\plugins\channel
+copy plugins\channel\demodbfm\%1\demodbfm.dll %2\plugins\channel
+copy plugins\channel\demodlora\%1\demodlora.dll %2\plugins\channel
+copy plugins\channel\demodnfm\%1\demodnfm.dll %2\plugins\channel
+copy plugins\channel\demodssb\%1\demodssb.dll %2\plugins\channel
+copy plugins\channel\demodwfm\%1\demodwfm.dll %2\plugins\channel
+copy plugins\channel\tcpsrc\%1\tcpsrc.dll %2\plugins\channel
+copy plugins\channel\udpsrc\%1\udpsrc.dll %2\plugins\channel
+copy plugins\samplesource\filesource\%1\inputfilesource.dll %2\plugins\sammplesource
+copy plugins\samplesource\sdrdaemon\%1\inputsdrdaemon.dll %2\plugins\sammplesource
