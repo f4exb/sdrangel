ba34b39309d3f3702f9467f323dbd0db2f941ade
f4exb <f4exb06@gmail.com>
2016-05-14 15:06:08 +0200

    Multi device support: added missing sanmple device UI class

---
+++ sdrbase/gui/samplingdevicecontrol.cpp
@@ -0,0 +1,48 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2015 Edouard Griffiths, F4EXB                                   //
+//                                                                               //
+// This program is free software; you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by          //
+// the Free Software Foundation as version 3 of the License, or                  //
+//                                                                               //
+// This program is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// GNU General Public License V3 for more details.                               //
+//                                                                               //
+// You should have received a copy of the GNU General Public License             //
+// along with this program. If not, see <http://www.gnu.org/licenses/>.          //
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "gui/samplingdevicecontrol.h"
+#include "gui/pluginsdialog.h"
+#include "plugin/pluginmanager.h"
+#include "ui_samplingdevicecontrol.h"
+
+
+SamplingDeviceControl::SamplingDeviceControl(QWidget* parent) :
+    QWidget(parent),
+    ui(new Ui::SamplingDeviceControl),
+    m_pluginManager(0)
+{
+    ui->setupUi(this);
+}
+
+SamplingDeviceControl::~SamplingDeviceControl()
+{
+    delete ui;
+}
+
+QComboBox *SamplingDeviceControl::getDeviceSelector()
+{
+    return ui->deviceSelect;
+}
+
+void SamplingDeviceControl::on_showLoadedPlugins_clicked(bool checked)
+{
+    if (m_pluginManager)
+    {
+        PluginsDialog pluginsDialog(m_pluginManager, this);
+        pluginsDialog.exec();
+    }
+}
---
+++ sdrbase/gui/samplingdevicecontrol.h
@@ -0,0 +1,52 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2015 Edouard Griffiths, F4EXB                                   //
+//                                                                               //
+// This program is free software; you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by          //
+// the Free Software Foundation as version 3 of the License, or                  //
+//                                                                               //
+// This program is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// GNU General Public License V3 for more details.                               //
+//                                                                               //
+// You should have received a copy of the GNU General Public License             //
+// along with this program. If not, see <http://www.gnu.org/licenses/>.          //
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef SDRBASE_GUI_SAMPLINGDEVICECONTROL_H_
+#define SDRBASE_GUI_SAMPLINGDEVICECONTROL_H_
+
+
+#include <QWidget>
+#include <QComboBox>
+
+#include "util/export.h"
+
+namespace Ui {
+    class SamplingDeviceControl;
+}
+
+class ChannelMarker;
+class PluginManager;
+
+class SDRANGEL_API SamplingDeviceControl : public QWidget {
+    Q_OBJECT
+
+public:
+    explicit SamplingDeviceControl(QWidget* parent = NULL);
+    ~SamplingDeviceControl();
+
+    void setPluginManager(PluginManager *pluginManager) { m_pluginManager = pluginManager; }
+    QComboBox *getDeviceSelector();
+
+private:
+    Ui::SamplingDeviceControl* ui;
+    PluginManager *m_pluginManager;
+
+private slots:
+    void on_showLoadedPlugins_clicked(bool checked);
+};
+
+
+#endif /* SDRBASE_GUI_SAMPLINGDEVICECONTROL_H_ */
