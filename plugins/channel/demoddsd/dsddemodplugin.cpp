c5ad25b19dfcf752ed8f5fa562928eca228291d8
f4exb <f4exb06@gmail.com>
2016-08-28 23:59:16 +0200

    Push versionto  2.1.3

--- plugins/channel/demoddsd/dsddemodplugin.cpp
+++ plugins/channel/demoddsd/dsddemodplugin.cpp
@@ -23,7 +23,7 @@
 
 const PluginDescriptor DSDDemodPlugin::m_pluginDescriptor = {
 	QString("DSD Demodulator"),
-	QString("2.1.2"),
+	QString("2.1.3"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
