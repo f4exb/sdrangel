e9f23ed2bfb0ee7c9004aedaa348e31d510757cb
f4exb <f4exb06@gmail.com>
2016-10-08 10:01:59 +0200

    Bumped to version 2.1.5 for DSDcc YSF voice on fix (1.5.1)

--- plugins/channelrx/demoddsd/dsddemodplugin.cpp
+++ plugins/channelrx/demoddsd/dsddemodplugin.cpp
@@ -25,7 +25,7 @@
 
 const PluginDescriptor DSDDemodPlugin::m_pluginDescriptor = {
 	QString("DSD Demodulator"),
-	QString("2.1.4"),
+	QString("2.1.5"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
