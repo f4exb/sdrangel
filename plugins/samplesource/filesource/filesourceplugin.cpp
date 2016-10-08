6f054a51d854b0823783e75cb7ae74cf3fbdf1ee
f4exb <f4exb06@gmail.com>
2016-10-08 06:25:58 +0200

    Bumped version to 2.1.5

--- plugins/samplesource/filesource/filesourceplugin.cpp
+++ plugins/samplesource/filesource/filesourceplugin.cpp
@@ -25,7 +25,7 @@
 
 const PluginDescriptor FileSourcePlugin::m_pluginDescriptor = {
 	QString("File source input"),
-	QString("2.0.0"),
+	QString("2.1.5"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
--- sdrbase/mainwindow.cpp
+++ sdrbase/mainwindow.cpp
@@ -335,7 +335,7 @@ void MainWindow::savePresetSettings(Preset* preset, int tabIndex)
 void MainWindow::createStatusBar()
 {
     QString qtVersionStr = QString("Qt %1 ").arg(QT_VERSION_STR);
-    m_showSystemWidget = new QLabel("SDRangel v2.1.4 " + qtVersionStr + QSysInfo::prettyProductName(), this);
+    m_showSystemWidget = new QLabel("SDRangel v2.1.5 " + qtVersionStr + QSysInfo::prettyProductName(), this);
     statusBar()->addPermanentWidget(m_showSystemWidget);
 
 	m_dateTimeWidget = new QLabel(tr("Date"), this);
