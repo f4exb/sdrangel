1a9c422906e2856e585257cfca16fe51323841e3
f4exb <f4exb06@gmail.com>
2017-05-18 00:05:08 +0200

    UDP source: use ValueDialZ for channel delta frequency value dial

--- plugins/channelrx/udpsrc/udpsrcgui.cpp
+++ plugins/channelrx/udpsrc/udpsrcgui.cpp
@@ -246,8 +246,9 @@ UDPSrcGUI::UDPSrcGUI(PluginAPI* pluginAPI, DeviceSourceAPI *deviceAPI, QWidget* 
 
 	ui->fmDeviation->setEnabled(false);
 
-	ui->deltaFrequency->setColorMapper(ColorMapper(ColorMapper::GrayGold));
-	ui->deltaFrequency->setValueRange(7, 0U, 9999999U);
+    ui->deltaFrequencyLabel->setText(QString("%1f").arg(QChar(0x94, 0x03)));
+    ui->deltaFrequency->setColorMapper(ColorMapper(ColorMapper::GrayGold));
+    ui->deltaFrequency->setValueRange(false, 7, -9999999, 9999999);
 
 	ui->glSpectrum->setCenterFrequency(0);
 	ui->glSpectrum->setSampleRate(ui->sampleRate->text().toInt());
@@ -357,8 +358,7 @@ void UDPSrcGUI::applySettings()
 		int boost = ui->boost->value();
 
 		setTitleColor(m_channelMarker.getColor());
-		ui->deltaFrequency->setValue(abs(m_channelMarker.getCenterFrequency()));
-		ui->deltaMinus->setChecked(m_channelMarker.getCenterFrequency() < 0);
+		ui->deltaFrequency->setValue(m_channelMarker.getCenterFrequency());
 		ui->sampleRate->setText(QString("%1").arg(outputSampleRate, 0));
 		ui->rfBandwidth->setText(QString("%1").arg(rfBandwidth, 0));
 		//ui->udpAddress->setText(m_udpAddress);
@@ -438,24 +438,9 @@ void UDPSrcGUI::applySettings()
 	}
 }
 
-void UDPSrcGUI::on_deltaMinus_toggled(bool minus)
+void UDPSrcGUI::on_deltaFrequency_changed(qint64 value)
 {
-	int deltaFrequency = m_channelMarker.getCenterFrequency();
-	bool minusDelta = (deltaFrequency < 0);
-
-	if (minus ^ minusDelta) // sign change
-	{
-		m_channelMarker.setCenterFrequency(-deltaFrequency);
-	}
-}
-
-void UDPSrcGUI::on_deltaFrequency_changed(quint64 value)
-{
-	if (ui->deltaMinus->isChecked()) {
-		m_channelMarker.setCenterFrequency(-value);
-	} else {
-		m_channelMarker.setCenterFrequency(value);
-	}
+    m_channelMarker.setCenterFrequency(value);
 }
 
 void UDPSrcGUI::on_sampleFormat_currentIndexChanged(int index)
