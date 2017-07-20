9c1c9cf850b636100c04eaec932cd297dc83c9bd
f4exb <f4exb06@gmail.com>
2017-07-20 23:42:29 +0200

    HackRF: added configure frquency delta common message

---
+++ devices/hackrf/devicehackrfshared.h
@@ -0,0 +1,47 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2017 Edouard Griffiths, F4EXB                                   //
+//                                                                               //
+// This program is free software; you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by          //
+// the Free Software Foundation as version 3 of the License, or                  //
+//                                                                               //
+// This program is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// GNU General Public License V3 for more details.                               //
+//                                                                               //
+// You should have received a copy of the GNU General Public License             //
+// along with this program. If not, see <http://www.gnu.org/licenses/>.          //
+///////////////////////////////////////////////////////////////////////////////////
+
+#ifndef DEVICES_HACKRF_DEVICEHACKRFSHARED_H_
+#define DEVICES_HACKRF_DEVICEHACKRFSHARED_H_
+
+#include "util/message.h"
+
+class DeviceHackRFShared
+{
+    class MsgConfigureFrequencyDelta : public Message
+    {
+        MESSAGE_CLASS_DECLARATION
+
+    public:
+        int getFrequencyDelta() const { return m_frequencyDelta; }
+
+        static MsgConfigureFrequencyDelta *create(int frequencyDelta)
+        {
+            return new MsgConfigureFrequencyDelta(frequencyDelta);
+        }
+
+    private:
+        int m_frequencyDelta;
+
+        MsgConfigureFrequencyDelta(int frequencyDelta) :
+            Message(),
+            m_frequencyDelta(frequencyDelta)
+        { }
+    };
+};
+
+
+#endif /* DEVICES_HACKRF_DEVICEHACKRFSHARED_H_ */
