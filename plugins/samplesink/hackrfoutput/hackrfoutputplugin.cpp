303cb1f3c38c535ba767499225cc08e65d9a118c
f4exb <f4exb06@gmail.com>
2017-07-22 04:50:38 +0200

    bumped version to 3.5.2

--- plugins/samplesink/hackrfoutput/hackrfoutputplugin.cpp
+++ plugins/samplesink/hackrfoutput/hackrfoutputplugin.cpp
@@ -29,7 +29,7 @@
 
 const PluginDescriptor HackRFOutputPlugin::m_pluginDescriptor = {
 	QString("HackRF Output"),
-	QString("3.5.1"),
+	QString("3.5.2"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
