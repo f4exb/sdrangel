eadb5003865b1ac3ff3a5eb36bd27b73826a0972
f4exb <f4exb06@gmail.com>
2017-08-12 12:07:51 +0200

    Cmake: make Boost mandatory and suggest fftw3f if not installed. Fixes issue #46

--- cmake/Modules/FindFFTW3F.cmake
+++ cmake/Modules/FindFFTW3F.cmake
@@ -7,34 +7,41 @@ INCLUDE(FindPkgConfig)
 PKG_CHECK_MODULES(PC_FFTW3F "fftw3f >= 3.0")
 
 FIND_PATH(
-	FFTW3F_INCLUDE_DIRS
-	NAMES fftw3.h
-	HINTS $ENV{FFTW3_DIR}/include
-		${PC_FFTW3F_INCLUDE_DIR}
-	PATHS /usr/local/include
-		  /usr/include
+    FFTW3F_INCLUDE_DIRS
+    NAMES fftw3.h
+    HINTS $ENV{FFTW3_DIR}/include
+        ${PC_FFTW3F_INCLUDE_DIR}
+    PATHS /usr/local/include
+          /usr/include
 )
 
 FIND_LIBRARY(
-	FFTW3F_LIBRARIES
-	NAMES fftw3f libfftw3f
-	HINTS $ENV{FFTW3_DIR}/lib
-		${PC_FFTW3F_LIBDIR}
-	PATHS /usr/local/lib
-		  /usr/lib
-		  /usr/lib64
+    FFTW3F_LIBRARIES
+    NAMES fftw3f libfftw3f
+    HINTS $ENV{FFTW3_DIR}/lib
+        ${PC_FFTW3F_LIBDIR}
+    PATHS /usr/local/lib
+          /usr/lib
+          /usr/lib64
 )
 
 FIND_LIBRARY(
-	FFTW3F_THREADS_LIBRARIES
-	NAMES fftw3f_threads libfftw3f_threads
-	HINTS $ENV{FFTW3_DIR}/lib
-		${PC_FFTW3F_LIBDIR}
-	PATHS /usr/local/lib
-		  /usr/lib
-		  /usr/lib64
+    FFTW3F_THREADS_LIBRARIES
+    NAMES fftw3f_threads libfftw3f_threads
+    HINTS $ENV{FFTW3_DIR}/lib
+        ${PC_FFTW3F_LIBDIR}
+    PATHS /usr/local/lib
+          /usr/lib
+          /usr/lib64
 )
 
 INCLUDE(FindPackageHandleStandardArgs)
 FIND_PACKAGE_HANDLE_STANDARD_ARGS(FFTW3F DEFAULT_MSG FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIRS)
 MARK_AS_ADVANCED(FFTW3F_LIBRARIES FFTW3F_INCLUDE_DIRS FFTW3F_THREADS_LIBRARIES)
+
+if(FFTW3F_LIBRARIES AND FFTW3F_INCLUDE_DIRS AND FFTW3F_THREADS_LIBRARIES)
+    message(STATUS "Found FFTW3F: ${FFTW3F_INCLUDE_DIRS}, ${FFTW3F_LIBRARIES}, ${FFTW3F_THREADS_LIBRARIES}")
+else()
+    set(CM256CC_FOUND FALSE CACHE INTERNAL "CM256CC found")
+    message(STATUS "FFTW3F not found. You should consider installing fftw3f")
+endif()
