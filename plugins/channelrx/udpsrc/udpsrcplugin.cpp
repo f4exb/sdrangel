061253a4ba8e26a76a9cdc2c24a35e18ec8cf2cd
f4exb <f4exb06@gmail.com>
2017-08-27 18:21:28 +0200

    UDP source plugin: updated version

--- plugins/channelrx/udpsrc/udpsrcplugin.cpp
+++ plugins/channelrx/udpsrc/udpsrcplugin.cpp
@@ -24,7 +24,7 @@
 
 const PluginDescriptor UDPSrcPlugin::m_pluginDescriptor = {
 	QString("UDP Channel Source"),
-	QString("3.6.0"),
+	QString("3.6.1"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
