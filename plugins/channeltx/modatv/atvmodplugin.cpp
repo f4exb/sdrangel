4252d76bc21add092cd11c11ccca3be680adce64
f4exb <f4exb06@gmail.com>
2017-10-22 01:54:15 +0200

    Bumped some plugins version

--- plugins/channeltx/modatv/atvmodplugin.cpp
+++ plugins/channeltx/modatv/atvmodplugin.cpp
@@ -23,7 +23,7 @@
 
 const PluginDescriptor ATVModPlugin::m_pluginDescriptor = {
     QString("ATV Modulator"),
-    QString("3.7.3"),
+    QString("3.7.5"),
     QString("(c) Edouard Griffiths, F4EXB"),
     QString("https://github.com/f4exb/sdrangel"),
     true,
