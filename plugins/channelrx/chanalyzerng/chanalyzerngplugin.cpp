44b49847f93efe23b6a7f4837010095f816d8b65
f4exb <f4exb06@gmail.com>
2017-11-11 05:14:58 +0100

    Bumped to version 3.8.3

--- plugins/channelrx/chanalyzerng/chanalyzerngplugin.cpp
+++ plugins/channelrx/chanalyzerng/chanalyzerngplugin.cpp
@@ -23,7 +23,7 @@
 
 const PluginDescriptor ChannelAnalyzerNGPlugin::m_pluginDescriptor = {
 	QString("Channel Analyzer NG"),
-	QString("3.8.2"),
+	QString("3.8.3"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
--- plugins/channelrx/demodssb/ssbplugin.cpp
+++ plugins/channelrx/demodssb/ssbplugin.cpp
@@ -8,7 +8,7 @@
 
 const PluginDescriptor SSBPlugin::m_pluginDescriptor = {
 	QString("SSB Demodulator"),
-	QString("3.8.2"),
+	QString("3.8.3"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
