62989b265031363aa1af7a79d00d5e3b3e25575f
f4exb <f4exb06@gmail.com>
2017-11-29 19:26:09 +0100

    Added installable udev rule files

---
+++ udev-rules/install.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+cp 52-airspy.rules /etc/udev/rules.d/
+cp 88-nuand.rules /etc/udev/rules.d/
+cp fcd.rules /etc/udev/rules.d/
+cp 53-hackrf.rules /etc/udev/rules.d/
+cp 64-limesuite.rules /etc/udev/rules.d/
+cp 53-adi-plutosdr-usb.rules /etc/udev/rules.d/
+cp rtl-sdr.rules /etc/udev/rules.d/
+cp mirisdr.rules /etc/udev/rules.d/
+
+udevadm control --reload-rules
+udevadm trigger
