22f174a2a48d53267d619b566156821803657afe
f4exb <f4exb06@gmail.com>
2017-12-17 11:09:51 +0100

    Server: basic sdrsrv library

---
+++ sdrsrv/maincore.cpp
@@ -0,0 +1,71 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2017 Edouard Griffiths, F4EXB.                                  //
+//                                                                               //
+// Swagger server adapter interface                                              //
+//                                                                               //
+// This program is free software; you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by          //
+// the Free Software Foundation as version 3 of the License, or                  //
+//                                                                               //
+// This program is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// GNU General Public License V3 for more details.                               //
+//                                                                               //
+// You should have received a copy of the GNU General Public License             //
+// along with this program. If not, see <http://www.gnu.org/licenses/>.          //
+///////////////////////////////////////////////////////////////////////////////////
+
+#include <QDebug>
+
+#include "dsp/dspengine.h"
+#include "dsp/dspdevicesourceengine.h"
+#include "plugin/pluginmanager.h"
+#include "loggerwithfile.h"
+#include "webapi/webapirequestmapper.h"
+#include "webapi/webapiserver.h"
+#include "webapi/webapiadaptersrv.h"
+
+#include "maincore.h"
+
+MainCore *MainCore::m_instance = 0;
+
+MainCore::MainCore(qtwebapp::LoggerWithFile *logger, const MainParser& parser) :
+    m_settings(),
+    m_masterTabIndex(-1),
+    m_dspEngine(DSPEngine::instance()),
+    m_lastEngineState((DSPDeviceSourceEngine::State)-1),
+    m_logger(logger)
+{
+    qDebug() << "MainCore::MainCore: start";
+
+    m_instance = this;
+    m_settings.setAudioDeviceInfo(&m_audioDeviceInfo);
+
+    m_pluginManager = new PluginManager(this);
+    m_pluginManager->loadPlugins(QString("pluginssrv"));
+
+    connect(&m_inputMessageQueue, SIGNAL(messageEnqueued()), this, SLOT(handleMessages()), Qt::QueuedConnection);
+    m_masterTimer.start(50);
+
+    m_apiAdapter = new WebAPIAdapterSrv(*this);
+    m_requestMapper = new WebAPIRequestMapper(QCoreApplication::instance());
+    m_requestMapper->setAdapter(m_apiAdapter);
+    m_apiServer = new WebAPIServer(parser.getServerAddress(), parser.getServerPort(), m_requestMapper);
+    m_apiServer->start();
+
+    qDebug() << "MainCore::MainCore: end";
+}
+
+MainCore::~MainCore()
+{
+    m_apiServer->stop();
+    delete m_apiServer;
+    delete m_requestMapper;
+    delete m_apiAdapter;
+
+    delete m_pluginManager;
+
+    qDebug() << "MainCore::~MainCore: end";
+    delete m_logger;
+}
--- sdrsrv/maincore.h
+++ sdrsrv/maincore.h
@@ -20,7 +20,13 @@
 #define SDRSRV_MAINCORE_H_
 
 #include <QObject>
+#include <QTimer>
+
+#include "settings/mainsettings.h"
+#include "util/message.h"
+#include "util/messagequeue.h"
 #include "util/export.h"
+#include "mainparser.h"
 
 class AudioDeviceInfo;
 class DSPEngine;
@@ -37,8 +43,41 @@ class WebAPIRequestMapper;
 class WebAPIServer;
 class WebAPIAdapterSrv; // TODO: create this class
 
+namespace qtwebapp {
+    class LoggerWithFile;
+}
+
 class SDRANGEL_API MainCore : public QObject {
     Q_OBJECT
+
+public:
+    explicit MainCore(qtwebapp::LoggerWithFile *logger, const MainParser& parser);
+    ~MainCore();
+    static MainCore *getInstance() { return m_instance; } // Main Core is de facto a singleton so this just returns its reference
+
+    MessageQueue* getInputMessageQueue() { return &m_inputMessageQueue; }
+
+    const QTimer& getMasterTimer() const { return m_masterTimer; }
+    const MainSettings& getMainSettings() const { return m_settings; }
+
+    friend class WebAPIAdapterSrv;
+
+private:
+    static MainCore *m_instance;
+    MessageQueue m_inputMessageQueue;
+    QTimer m_masterTimer;
+    MainSettings m_settings;
+    int m_masterTabIndex;
+    DSPEngine* m_dspEngine;
+    PluginManager* m_pluginManager;
+    int m_lastEngineState;
+    AudioDeviceInfo m_audioDeviceInfo;
+
+    qtwebapp::LoggerWithFile *m_logger;
+
+    WebAPIRequestMapper *m_requestMapper;
+    WebAPIServer *m_apiServer;
+    WebAPIAdapterSrv *m_apiAdapter;
 };
 
 
