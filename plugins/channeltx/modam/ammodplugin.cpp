4444084b7afef2974c0a9ad0ff67dca8a90c955d
f4exb <f4exb06@gmail.com>
2018-01-09 01:23:27 +0100

    Updated version of all modulators

--- plugins/channeltx/modam/ammodplugin.cpp
+++ plugins/channeltx/modam/ammodplugin.cpp
@@ -24,7 +24,7 @@
 
 const PluginDescriptor AMModPlugin::m_pluginDescriptor = {
     QString("AM Modulator"),
-    QString("3.9.0"),
+    QString("3.10.1"),
     QString("(c) Edouard Griffiths, F4EXB"),
     QString("https://github.com/f4exb/sdrangel"),
     true,
--- plugins/channeltx/modssb/ssbmodplugin.cpp
+++ plugins/channeltx/modssb/ssbmodplugin.cpp
@@ -24,7 +24,7 @@
 
 const PluginDescriptor SSBModPlugin::m_pluginDescriptor = {
     QString("SSB Modulator"),
-    QString("3.9.0"),
+    QString("3.10.1"),
     QString("(c) Edouard Griffiths, F4EXB"),
     QString("https://github.com/f4exb/sdrangel"),
     true,
