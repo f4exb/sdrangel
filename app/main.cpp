400c712ca8bf10729f4dbe61d0b24c4d6a3eb708
f4exb <f4exb06@gmail.com>
2018-02-25 00:31:19 +0100

    Bumped to v3.13.0

--- app/main.cpp
+++ app/main.cpp
@@ -35,7 +35,7 @@ static int runQtApplication(int argc, char* argv[], qtwebapp::LoggerWithFile *lo
 */
 	QCoreApplication::setOrganizationName("f4exb");
 	QCoreApplication::setApplicationName("SDRangel");
-	QCoreApplication::setApplicationVersion("3.12.0");
+	QCoreApplication::setApplicationVersion("3.13.0");
 
 #if 1
 	qApp->setStyle(QStyleFactory::create("fusion"));
