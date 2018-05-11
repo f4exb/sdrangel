79c22f11a59a05ed7f01fe7d43977f3a441f3599
f4exb <f4exb06@gmail.com>
2018-05-11 11:14:02 +0200

    BladeRF: updated library version for Debian build

--- libbladerf/include/version.h
+++ libbladerf/include/version.h
@@ -1,7 +1,7 @@
 #ifndef VERSION_H__
 #define VERSION_H__
 
-#define LIBBLADERF_VERSION       "1.7.2-git-unknown"
+#define LIBBLADERF_VERSION       "1.9.0-git-23c6379e"
 
 #define LIBBLADERF_VERSION_MAJOR 1
 #define LIBBLADERF_VERSION_MINOR 7
