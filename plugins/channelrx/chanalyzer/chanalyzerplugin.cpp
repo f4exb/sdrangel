b0eb2b6c1dfc095d4ad567ea8cb48c6319edc839
f4exb <f4exb06@gmail.com>
2018-07-08 15:06:33 +0200

    Upgraded Channel analyzer version and updaed Debian changelog

--- plugins/channelrx/chanalyzer/chanalyzerplugin.cpp
+++ plugins/channelrx/chanalyzer/chanalyzerplugin.cpp
@@ -23,7 +23,7 @@
 
 const PluginDescriptor ChannelAnalyzerPlugin::m_pluginDescriptor = {
 	QString("Channel Analyzer"),
-	QString("4.0.2"),
+	QString("4.0.3"),
 	QString("(c) Edouard Griffiths, F4EXB"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
