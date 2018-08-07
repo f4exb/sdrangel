b9fb572fab71bfe6a35731fc137af0025350c6a9
f4exb <f4exb06@gmail.com>
2018-08-07 08:46:07 +0200

    Removed obsolete elements in windows install script

--- windows.install.bat
+++ windows.install.bat
@@ -21,7 +21,7 @@ copy librtlsdr\%1\librtlsdr.dll %2
 copy libairspy\%1\libairspy.dll %2
 copy libairspyhf\%1\libairspyhf.dll %2
 copy libbladerf\%1\libbladerf.dll %2
-copy libsqlite3\%1\libsqlite3.dll %2
+REM copy libsqlite3\%1\libsqlite3.dll %2
 copy liblimesuite\%1\liblimesuite.dll %2
 copy libiio\%1\libiio.dll %2
 copy %libusbdir%\dll\libusb-1.0.dll %2
@@ -38,7 +38,7 @@ mkdir %2\plugins\channeltx
 mkdir %2\plugins\samplesource
 mkdir %2\plugins\samplesink
 copy plugins\channelrx\chanalyzer\%1\chanalyzer.dll %2\plugins\channelrx
-copy plugins\channelrx\chanalyzerng\%1\chanalyzerng.dll %2\plugins\channelrx
+REM copy plugins\channelrx\chanalyzerng\%1\chanalyzerng.dll %2\plugins\channelrx
 copy plugins\channelrx\demodam\%1\demodam.dll %2\plugins\channelrx
 copy plugins\channelrx\demodatv\%1\demodatv.dll %2\plugins\channelrx
 copy plugins\channelrx\demodbfm\%1\demodbfm.dll %2\plugins\channelrx
@@ -47,7 +47,7 @@ copy plugins\channelrx\demodlora\%1\demodlora.dll %2\plugins\channelrx
 copy plugins\channelrx\demodnfm\%1\demodnfm.dll %2\plugins\channelrx
 copy plugins\channelrx\demodssb\%1\demodssb.dll %2\plugins\channelrx
 copy plugins\channelrx\demodwfm\%1\demodwfm.dll %2\plugins\channelrx
-copy plugins\channelrx\tcpsrc\%1\tcpsrc.dll %2\plugins\channelrx
+REM copy plugins\channelrx\tcpsrc\%1\tcpsrc.dll %2\plugins\channelrx
 REM copy plugins\channelrx\udpsrc\%1\udpsrc.dll %2\plugins\channelrx
 copy plugins\channeltx\modam\%1\modam.dll %2\plugins\channeltx
 REM copy plugins\channeltx\modatv\%1\modatv.dll %2\plugins\channeltx
