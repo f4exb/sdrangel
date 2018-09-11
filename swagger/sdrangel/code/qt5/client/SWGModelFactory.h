9cfaf47a008db72be9eebefd323c4bddda0f00a1
f4exb <f4exb06@gmail.com>
2018-09-11 14:52:16 +0200

    REST API: removed old SDRDaemon code

--- swagger/sdrangel/code/qt5/client/SWGModelFactory.h
+++ swagger/sdrangel/code/qt5/client/SWGModelFactory.h
@@ -1,6 +1,6 @@
 /**
  * SDRangel
- * This is the web REST/JSON API of SDRangel SDR software. SDRangel is an Open Source Qt5/OpenGL 3.0+ (4.3+ in Windows) GUI and server Software Defined Radio and signal analyzer in software. It supports Airspy, BladeRF, HackRF, LimeSDR, PlutoSDR, RTL-SDR, SDRplay RSP1 and FunCube     ---   Limitations and specifcities:       * In SDRangel GUI the first Rx device set cannot be deleted. Conversely the server starts with no device sets and its number of device sets can be reduced to zero by as many calls as necessary to /sdrangel/deviceset with DELETE method.   * Preset import and export from/to file is a server only feature.   * Device set focus is a GUI only feature.   * The following channels are not implemented (status 501 is returned): ATV and DATV demodulators, Channel Analyzer NG, LoRa demodulator   * The device settings and report structures contains only the sub-structure corresponding to the device type. The DeviceSettings and DeviceReport structures documented here shows all of them but only one will be or should be present at a time   * The channel settings and report structures contains only the sub-structure corresponding to the channel type. The ChannelSettings and ChannelReport structures documented here shows all of them but only one will be or should be present at a time    --- 
+ * This is the web REST/JSON API of SDRangel SDR software. SDRangel is an Open Source Qt5/OpenGL 3.0+ (4.3+ in Windows) GUI and server Software Defined Radio and signal analyzer in software. It supports Airspy, BladeRF, HackRF, LimeSDR, PlutoSDR, RTL-SDR, SDRplay RSP1 and FunCube    ---   Limitations and specifcities:    * In SDRangel GUI the first Rx device set cannot be deleted. Conversely the server starts with no device sets and its number of device sets can be reduced to zero by as many calls as necessary to /sdrangel/deviceset with DELETE method.   * Preset import and export from/to file is a server only feature.   * Device set focus is a GUI only feature.   * The following channels are not implemented (status 501 is returned): ATV and DATV demodulators, Channel Analyzer NG, LoRa demodulator   * The device settings and report structures contains only the sub-structure corresponding to the device type. The DeviceSettings and DeviceReport structures documented here shows all of them but only one will be or should be present at a time   * The channel settings and report structures contains only the sub-structure corresponding to the channel type. The ChannelSettings and ChannelReport structures documented here shows all of them but only one will be or should be present at a time    --- 
  *
  * OpenAPI spec version: 4.1.0
  * Contact: f4exb06@gmail.com
@@ -45,7 +45,6 @@
 #include "SWGDaemonSinkSettings.h"
 #include "SWGDaemonSourceReport.h"
 #include "SWGDaemonSourceSettings.h"
-#include "SWGDaemonSummaryResponse.h"
 #include "SWGDeviceListItem.h"
 #include "SWGDeviceReport.h"
 #include "SWGDeviceSet.h"
@@ -210,9 +209,6 @@ namespace SWGSDRangel {
     }
     if(QString("SWGDaemonSourceSettings").compare(type) == 0) {
       return new SWGDaemonSourceSettings();
-    }
-    if(QString("SWGDaemonSummaryResponse").compare(type) == 0) {
-      return new SWGDaemonSummaryResponse();
     }
     if(QString("SWGDeviceListItem").compare(type) == 0) {
       return new SWGDeviceListItem();
