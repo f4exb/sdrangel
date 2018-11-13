b8d894894e5c03852fadc77363164114a116d4c1
f4exb <f4exb06@gmail.com>
2018-11-13 11:16:49 +0100

    Windows: MSVC2017: adapt testsource plugin module

--- plugins/samplesource/testsource/testsourcethread.cpp
+++ plugins/samplesource/testsource/testsourcethread.cpp
@@ -14,6 +14,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.          //
 ///////////////////////////////////////////////////////////////////////////////////
 
+#define _USE_MATH_DEFINES
+#include <math.h>
 #include <stdio.h>
 #include <errno.h>
 #include "testsourcethread.h"
