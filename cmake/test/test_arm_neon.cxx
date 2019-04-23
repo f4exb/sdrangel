3c8eb44abf968ab8b65c136e95f7f8077ad83e9c
f4exb <f4exb06@gmail.com>
2019-04-23 09:49:58 +0200

    cmake armv7 SIMD test programs: avoid possible core dumps by catching SIGILL

--- cmake/test/test_arm_neon.cxx
+++ cmake/test/test_arm_neon.cxx
@@ -1,8 +1,15 @@
 #include <arm_neon.h>
 #include <stdint.h>
+#include <stdlib.h>
+#include <signal.h>
+
+void signalHandler(int signum) {
+    exit(signum); // SIGILL = 4
+}
 
 int main(int argc, char* argv[])
 {
+    signal(SIGILL, signalHandler);
 	uint32x4_t x={0};
 	x=veorq_u32(x,x);
 	return 0;
