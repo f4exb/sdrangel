301c580b56b6a5682db9b2882f2d3d3e168608bb
Davide Gerhard <rainbow@irh.it>
2019-05-22 17:16:06 +0200

    move to debhelper tool to create debian package

--- cmake/travis-ci/build_sdrangel.sh
+++ cmake/travis-ci/build_sdrangel.sh
@@ -1,21 +1,26 @@
 #!/bin/sh
 
-if [[ "$TRAVIS_OS_NAME" == "osx" ]]; then
+if [ "${TRAVIS_OS_NAME}" == "osx" ]; then
   JOBS=$(sysctl -n hw.ncpu)
-elif [[ "$TRAVIS_OS_NAME" == "linux" ]]; then
+elif [ "${TRAVIS_OS_NAME}" == "linux" ]; then
   JOBS=$(nproc --all)
 else
   JOBS=1
 fi
 
-mkdir build && cd build
-cmake .. "${CMAKE_CUSTOM_OPTIONS}"
 
-case "${CMAKE_CUSTOM_OPTIONS}" in
-  *BUNDLE=ON*)
-    make -j${JOBS} package
-  ;;
-  *)
-    make -j${JOBS}
-  ;;
-esac
+if [ "${TRAVIS_OS_NAME}" == "linux" ]; then
+  debuild -i -us -uc -b
+else
+  mkdir build && cd build
+  cmake .. "${CMAKE_CUSTOM_OPTIONS}"
+
+  case "${CMAKE_CUSTOM_OPTIONS}" in
+    *BUNDLE=ON*)
+      make -j${JOBS} package
+    ;;
+    *)
+      make -j${JOBS}
+    ;;
+  esac
+fi
