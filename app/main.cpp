966ea1ed07c465bd0ea51a4f1a6c0848086dc30e
f4exb <f4exb06@gmail.com>
2019-05-31 09:00:37 +0200

    Debian: updated changelog. Bumped version

--- app/main.cpp
+++ app/main.cpp
@@ -27,7 +27,7 @@
 #include "mainwindow.h"
 #include "dsp/dsptypes.h"
 #ifdef _MSC_VER
-#define GIT_COMMIT_HASH_STR "v4.8.1"
+#define GIT_COMMIT_HASH_STR "v4.8.2"
 #else
 #include "sdrangel_version.h"
 #endif
