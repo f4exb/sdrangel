f528347fd580174ccde9a02b392e6fb546a59adc
f4exb <f4exb06@gmail.com>
2020-04-09 06:33:04 +0200

    AM demod: fixed initialization of squelch counter. Fixes issue #502

--- plugins/channelrx/demodam/amdemodsink.cpp
+++ plugins/channelrx/demodam/amdemodsink.cpp
@@ -29,6 +29,7 @@
 AMDemodSink::AMDemodSink() :
         m_channelSampleRate(48000),
         m_channelFrequencyOffset(0),
+        m_squelchCount(0),
         m_squelchOpen(false),
         m_squelchDelayLine(9600),
         m_magsqSum(0.0f),
