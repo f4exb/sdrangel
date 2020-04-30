d9ea8d4abca415822930be4951a270e6e9adacf8
Sergei I. Korolev <exception0x0d@gmail.com>
2020-04-30 15:19:19 +0300

    Added more flexible build options

--- cmake/Modules/FindCM256cc.cmake
+++ cmake/Modules/FindCM256cc.cmake
@@ -1,36 +1,38 @@
-INCLUDE(FindPkgConfig)
-PKG_CHECK_MODULES(PC_CM256cc "libcm256cc")
+if (NOT CM256CC_FOUND)
+    INCLUDE(FindPkgConfig)
+    PKG_CHECK_MODULES(PC_CM256cc "libcm256cc")
 
-FIND_PATH(CM256CC_INCLUDE_DIR
-    NAMES cm256cc/cm256.h
-    HINTS ${CM256CC_DIR}/include
-          ${PC_CM256CC_INCLUDE_DIR}
-          ${CMAKE_INSTALL_PREFIX}/include
-    PATHS /usr/local/include
-          /usr/include
-)
+    FIND_PATH(CM256CC_INCLUDE_DIR
+        NAMES cm256cc/cm256.h
+        HINTS ${CM256CC_DIR}/include
+              ${PC_CM256CC_INCLUDE_DIR}
+              ${CMAKE_INSTALL_PREFIX}/include
+        PATHS /usr/local/include
+              /usr/include
+    )
 
-FIND_LIBRARY(CM256CC_LIBRARIES
-    NAMES cm256cc libcm256cc
-    HINTS ${CM256CC_DIR}/lib
-          ${CM256CC_DIR}/lib64
-          ${PC_CM256CC_LIBDIR}
-          ${CMAKE_INSTALL_PREFIX}/lib
-          ${CMAKE_INSTALL_PREFIX}/lib64
-    PATHS /usr/local/lib
-          /usr/local/lib64
-          /usr/lib
-          /usr/lib64
-)
+    FIND_LIBRARY(CM256CC_LIBRARIES
+        NAMES cm256cc libcm256cc
+        HINTS ${CM256CC_DIR}/lib
+              ${CM256CC_DIR}/lib64
+              ${PC_CM256CC_LIBDIR}
+              ${CMAKE_INSTALL_PREFIX}/lib
+              ${CMAKE_INSTALL_PREFIX}/lib64
+        PATHS /usr/local/lib
+              /usr/local/lib64
+              /usr/lib
+              /usr/lib64
+    )
 
-if(CM256CC_INCLUDE_DIR AND CM256CC_LIBRARIES)
-    set(CM256CC_FOUND TRUE CACHE INTERNAL "CM256CC found")
-    message(STATUS "Found CM256cc: ${CM256CC_INCLUDE_DIR}, ${CM256CC_LIBRARIES}")
-else(CM256CC_INCLUDE_DIR AND CM256CC_LIBRARIES)
-    set(CM256CC_FOUND FALSE CACHE INTERNAL "CM256CC found")
-    message(STATUS "CM256cc not found")
-endif(CM256CC_INCLUDE_DIR AND CM256CC_LIBRARIES)
+    if(CM256CC_INCLUDE_DIR AND CM256CC_LIBRARIES)
+        set(CM256CC_FOUND TRUE CACHE INTERNAL "CM256CC found")
+        message(STATUS "Found CM256cc: ${CM256CC_INCLUDE_DIR}, ${CM256CC_LIBRARIES}")
+    else(CM256CC_INCLUDE_DIR AND CM256CC_LIBRARIES)
+        set(CM256CC_FOUND FALSE CACHE INTERNAL "CM256CC found")
+        message(STATUS "CM256cc not found")
+    endif(CM256CC_INCLUDE_DIR AND CM256CC_LIBRARIES)
 
-INCLUDE(FindPackageHandleStandardArgs)
-FIND_PACKAGE_HANDLE_STANDARD_ARGS(CM256CC DEFAULT_MSG CM256CC_LIBRARIES CM256CC_INCLUDE_DIR)
-MARK_AS_ADVANCED(CM256CC_LIBRARIES CM256CC_INCLUDE_DIR)
+    INCLUDE(FindPackageHandleStandardArgs)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(CM256CC DEFAULT_MSG CM256CC_LIBRARIES CM256CC_INCLUDE_DIR)
+    MARK_AS_ADVANCED(CM256CC_LIBRARIES CM256CC_INCLUDE_DIR)
+endif (NOT CM256CC_FOUND)
--- cmake/Modules/FindSerialDV.cmake
+++ cmake/Modules/FindSerialDV.cmake
@@ -1,33 +1,34 @@
 # Find serialDV
+if (NOT LIBSERIALDV_FOUND)
+    find_path(LIBSERIALDV_INCLUDE_DIR
+      NAMES dvcontroller.h
+      HINTS ${SERIALDV_DIR}/include/serialdv
+      PATHS /usr/include/serialdv
+            /usr/local/include/serialdv
+    )
 
-find_path(LIBSERIALDV_INCLUDE_DIR
-  NAMES dvcontroller.h
-  HINTS ${SERIALDV_DIR}/include/serialdv
-  PATHS /usr/include/serialdv
-        /usr/local/include/serialdv
-)
+    set(LIBSERIAL_NAMES ${LIBSERIAL_NAMES} serialdv libserialdv)
 
-set(LIBSERIAL_NAMES ${LIBSERIAL_NAMES} serialdv libserialdv)
+    find_library(LIBSERIALDV_LIBRARY
+      NAMES serialdv
+      HINTS ${SERIALDV_DIR}/lib
+      PATHS /usr/lib
+            /usr/local/lib
+    )
 
-find_library(LIBSERIALDV_LIBRARY
-  NAMES serialdv
-  HINTS ${SERIALDV_DIR}/lib
-  PATHS /usr/lib
-        /usr/local/lib
-)
+    if (LIBSERIALDV_INCLUDE_DIR AND LIBSERIALDV_LIBRARY)
+        set(LIBSERIALDV_FOUND TRUE CACHE INTERNAL "")
 
-if (LIBSERIALDV_INCLUDE_DIR AND LIBSERIALDV_LIBRARY)
-    set(LIBSERIALDV_FOUND TRUE)
-endif (LIBSERIALDV_INCLUDE_DIR AND LIBSERIALDV_LIBRARY)
+        if (NOT SerialDV_FIND_QUIETLY)
+            message (STATUS "Found libserialdv: ${LIBSERIALDV_INCLUDE_DIR}, ${LIBSERIALDV_LIBRARY}")
+        endif (NOT SerialDV_FIND_QUIETLY)
+    else (LIBSERIALDV_INCLUDE_DIR AND LIBSERIALDV_LIBRARY)
+        set(LIBSERIALDV_FOUND FALSE CACHE INTERNAL "")
 
-if (LIBSERIALDV_FOUND)
-    if (NOT SerialDV_FIND_QUIETLY)
-        message (STATUS "Found libserialdv: ${LIBSERIALDV_INCLUDE_DIR}, ${LIBSERIALDV_LIBRARY}")
-    endif (NOT SerialDV_FIND_QUIETLY)
-else (LIBSERIALDV_FOUND)
-    if (SerialDV_FIND_REQUIRED)
-        message (FATAL_ERROR "Could not find SerialDV")
-    endif (SerialDV_FIND_REQUIRED)
-endif (LIBSERIALDV_FOUND)
+        if (SerialDV_FIND_REQUIRED)
+            message (FATAL_ERROR "Could not find SerialDV")
+        endif (SerialDV_FIND_REQUIRED)
+    endif (LIBSERIALDV_INCLUDE_DIR AND LIBSERIALDV_LIBRARY)
 
-mark_as_advanced(LIBSERIALDV_INCLUDE_DIR LIBSERIALDV_LIBRARY)
+    mark_as_advanced(LIBSERIALDV_INCLUDE_DIR LIBSERIALDV_LIBRARY)
+endif (NOT LIBSERIALDV_FOUND)
