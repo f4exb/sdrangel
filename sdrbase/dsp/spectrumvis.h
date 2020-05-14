19ece206cfb0ea5b3983e509e3e51a70f257bb3c
f4exb <f4exb06@gmail.com>
2020-05-14 22:14:11 +0200

    SpectrumVis: corrected export define

--- sdrbase/dsp/spectrumvis.h
+++ sdrbase/dsp/spectrumvis.h
@@ -43,10 +43,10 @@ namespace SWGSDRangel {
     class SWGSuccessResponse;
 };
 
-class SDRGUI_API SpectrumVis : public BasebandSampleSink {
+class SDRBASE_API SpectrumVis : public BasebandSampleSink {
 
 public:
-    class MsgConfigureSpectrumVis : public Message {
+    class SDRBASE_API MsgConfigureSpectrumVis : public Message {
         MESSAGE_CLASS_DECLARATION
 
     public:
@@ -68,7 +68,7 @@ public:
         { }
     };
 
-    class MsgConfigureWSpectrumOpenClose : public Message
+    class SDRBASE_API MsgConfigureWSpectrumOpenClose : public Message
     {
 		MESSAGE_CLASS_DECLARATION
 
