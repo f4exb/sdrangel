62f451e645b9967dc532b9ec047d90ba26aadd27
f4exb <f4exb06@gmail.com>
2020-06-27 10:33:11 +0200

    Fixed swagger code generation script

--- swagger/sdrangel/generate.sh
+++ swagger/sdrangel/generate.sh
@@ -7,4 +7,4 @@ ${CODEGEN} generate -i api/swagger/swagger.yaml -l qt5cpp -c qt5cpp-config.json 
 ${CODEGEN} generate -i api/swagger/swagger.yaml -l html2 -c html2-config.json -o code/html2
 cp -v code/html2/index.html ${SDRANGEL_SRC}/sdrbase/resources/webapi/doc/html2
 cp -av api/swagger/ ${SDRANGEL_SRC}/sdrbase/resources/webapi/doc
-find ${SDRANGEL_SRC}/sdrbase/resources/webapi/doc/swagger -name \*.yaml -exec sed -i 's/http:\/\/localhost:8081\/api\/swagger\/include/\/doc\/swagger\/include/g' {} \;
+find ${SDRANGEL_SRC}/sdrbase/resources/webapi/doc/swagger -name \*.yaml -exec sed -i 's/http:\/\/swgserver:8081\/api\/swagger\/include/\/doc\/swagger\/include/g' {} \;
