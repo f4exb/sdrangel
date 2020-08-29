07a901942b5a4be2fa30321107301b06e01f5067
f4exb <f4exb06@gmail.com>
2020-08-29 22:32:11 +0200

    Updated versions and changelogs

--- plugins/channelrx/demoddatv/datvdemodplugin.cpp
+++ plugins/channelrx/demoddatv/datvdemodplugin.cpp
@@ -30,7 +30,7 @@ const PluginDescriptor DATVDemodPlugin::m_ptrPluginDescriptor =
 {
     DATVDemod::m_channelId,
     QString("DATV Demodulator"),
-    QString("4.14.14"),
+    QString("4.15.4"),
     QString("(c) F4HKW for SDRAngel using LeanSDR framework (c) F4DAV"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
