8c23528e837a8fed8d08cfa40d574f49470f23fa
f4exb <f4exb06@gmail.com>
2020-09-21 19:45:14 +0200

    DATV demod: fixed incorrect frequency update when spectrum point and click is used. Fixes issue #637

--- plugins/channelrx/demoddatv/datvdemodplugin.cpp
+++ plugins/channelrx/demoddatv/datvdemodplugin.cpp
@@ -30,7 +30,7 @@ const PluginDescriptor DATVDemodPlugin::m_ptrPluginDescriptor =
 {
     DATVDemod::m_channelId,
     QString("DATV Demodulator"),
-    QString("4.12.3"),
+    QString("4.16.2"),
     QString("(c) F4HKW for SDRAngel using LeanSDR framework (c) F4DAV"),
 	QString("https://github.com/f4exb/sdrangel"),
 	true,
