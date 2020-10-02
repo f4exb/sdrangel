c895363e3b41deb45fc3534c1a004b1adf48f28f
Jon Beniston <jon@beniston.com>
2020-10-02 23:22:03 +0100

    Update FindUHD rules

--- cmake/Modules/FindUHD.cmake
+++ cmake/Modules/FindUHD.cmake
@@ -1,29 +1,29 @@
 IF(NOT UHD_FOUND)
     INCLUDE(FindPkgConfig)
     PKG_CHECK_MODULES(PC_UHD uhd)
-    
+
     FIND_PATH(
-        UHD_INCLUDE_DIRS
+        UHD_INCLUDE_DIR
         NAMES uhd.h
-        HINTS $ENV{UHD_DIR}/include
+        HINTS ${UHD_DIR}/include
         PATHS /usr/local/include
               /usr/include
     )
-    
+
     FIND_LIBRARY(
         UHD_LIBRARIES
         NAMES uhd
-        HINTS $ENV{UHD_DIR}/lib
+        HINTS ${UHD_DIR}/lib
         PATHS /usr/local/lib
               /usr/lib
               /usr/lib64
     )
-    
+
     message(STATUS "UHD LIBRARIES " ${UHD_LIBRARIES})
-    message(STATUS "UHD INCLUDE DIRS " ${UHD_INCLUDE_DIRS})
-    
+    message(STATUS "UHD INCLUDE DIRS " ${UHD_INCLUDE_DIR})
+
     INCLUDE(FindPackageHandleStandardArgs)
-    FIND_PACKAGE_HANDLE_STANDARD_ARGS(UHD DEFAULT_MSG UHD_LIBRARIES UHD_INCLUDE_DIRS)
-    MARK_AS_ADVANCED(UHD_LIBRARIES UHD_INCLUDE_DIRS)
+    FIND_PACKAGE_HANDLE_STANDARD_ARGS(UHD DEFAULT_MSG UHD_LIBRARIES UHD_INCLUDE_DIR)
+    MARK_AS_ADVANCED(UHD_LIBRARIES UHD_INCLUDE_DIR)
 
 ENDIF(NOT UHD_FOUND)
