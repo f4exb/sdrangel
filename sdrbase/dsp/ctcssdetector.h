ef1a7ef8d2608272877f62a2f1bb7ea2188d4017
f4exb <f4exb06@gmail.com>
2020-11-01 08:23:50 +0100

    CTCSS detector: fixed crash on destructor. Fixes #680

--- sdrbase/dsp/ctcssdetector.h
+++ sdrbase/dsp/ctcssdetector.h
@@ -1,16 +1,28 @@
-/*
- * ctcssdetector.h
- *
- *  Created on: Jun 16, 2015
- *      Author: f4exb
- *      See: http://www.embedded.com/design/connectivity/4025660/Detecting-CTCSS-tones-with-Goertzel-s-algorithm
- */
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2015-2020 Edouard Griffiths, F4EXB                                                         //
+//                                                                                                          //
+// See: http://www.embedded.com/design/connectivity/4025660/Detecting-CTCSS-tones-with-Goertzel-s-algorithm //
+//                                                                                                          //
+// This program is free software; you can redistribute it and/or modify                                     //
+// it under the terms of the GNU General Public License as published by                                     //
+// the Free Software Foundation as version 3 of the License, or                                             //
+// (at your option) any later version.                                                                      //
+//                                                                                                          //
+// This program is distributed in the hope that it will be useful,                                          //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                                           //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                                             //
+// GNU General Public License V3 for more details.                                                          //
+//                                                                                                          //
+// You should have received a copy of the GNU General Public License                                        //
+// along with this program. If not, see <http://www.gnu.org/licenses/>.                                     //
+//////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 #ifndef INCLUDE_GPL_DSP_CTCSSDETECTOR_H_
 #define INCLUDE_GPL_DSP_CTCSSDETECTOR_H_
 
 #include "dsp/dsptypes.h"
 #include "export.h"
+#include "ctcssfrequencies.h"
 
 /** CTCSSDetector: Continuous Tone Coded Squelch System
  * tone detector class based on the Modified Goertzel
@@ -18,49 +30,45 @@
  */
 class SDRBASE_API CTCSSDetector {
 public:
-    // Constructors and Destructor
     CTCSSDetector();
-    // allows user defined CTCSS tone set
-    CTCSSDetector(int _nTones, Real *tones);
+    CTCSSDetector(int _nTones, Real *tones); //!< allows user defined CTCSS tone set
     virtual ~CTCSSDetector();
 
     // setup the basic parameters and coefficients
     void setCoefficients(
-    		int zN,            // the algorithm "block"  size
-			int SampleRate);  // input signal sample rate
+    		int N,          // the algorithm "block"  size
+			int sampleRate  // input signal sample rate
+    );
 
     // set the detection threshold
     void setThreshold(double thold);
 
-    // analyze a sample set and optionally filter
-    // the tone frequencies.
+    // analyze a sample set and optionally filter the tone frequencies.
     bool analyze(Real *sample); // input signal sample
 
     // get the number of defined tones.
     int getNTones() const {
-    	return nTones;
+    	return CTCSSFrequencies::m_nbFreqs;
     }
 
     // get the tone set
-    const Real *getToneSet() const
-    {
-    	return toneSet;
+    const Real *getToneSet() const {
+    	return CTCSSFrequencies::m_Freqs;
     }
 
     // get the currently detected tone, if any
     bool getDetectedTone(int &maxTone) const
     {
-    	maxTone = maxPowerIndex;
-    	return toneDetected;
+    	maxTone = m_maxPowerIndex;
+    	return m_toneDetected;
     }
 
-    // Get the max power at the detected tone.
-    Real getMaxPower() const
-    {
-    	return maxPower;
+    // Get the max m_power at the detected tone.
+    Real getMaxPower() const {
+    	return m_maxPower;
     }
 
-    void reset();                       // reset the analysis algorithm
+    void reset(); // reset the analysis algorithm
 
 protected:
     // Override these to change behavior of the detector
@@ -70,19 +78,17 @@ protected:
     void feedForward();
 
 private:
-    int N;
-    int sampleRate;
-    int nTones;
-    int samplesProcessed;
-    int maxPowerIndex;
-    bool toneDetected;
-    Real maxPower;
-    Real *k;
-    Real *coef;
-    const float *toneSet;
-    Real *u0;
-    Real *u1;
-    Real *power;
+    int m_N;
+    int m_sampleRate;
+    int m_samplesProcessed;
+    int m_maxPowerIndex;
+    bool m_toneDetected;
+    Real m_maxPower;
+    Real *m_k;
+    Real *m_coef;
+    Real *m_u0;
+    Real *m_u1;
+    Real *m_power;
 };
 
 
