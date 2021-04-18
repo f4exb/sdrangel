a5796c711673efc4d0e41a73a95d455d55aec129
f4exb <f4exb06@gmail.com>
2021-04-18 22:57:54 +0200

    Updated versions and changelogs

--- plugins/channelrx/demodnfm/nfmplugin.cpp
+++ plugins/channelrx/demodnfm/nfmplugin.cpp
@@ -12,7 +12,7 @@
 const PluginDescriptor NFMPlugin::m_pluginDescriptor = {
     NFMDemod::m_channelId,
 	QStringLiteral("NFM Demodulator"),
-	QStringLiteral("6.6.1"),
+	QStringLiteral("6.10.0"),
 	QStringLiteral("(c) Edouard Griffiths, F4EXB"),
 	QStringLiteral("https://github.com/f4exb/sdrangel"),
 	true,
--- plugins/channelrx/demodvor/vordemodplugin.cpp
+++ plugins/channelrx/demodvor/vordemodplugin.cpp
@@ -29,7 +29,7 @@
 const PluginDescriptor VORDemodPlugin::m_pluginDescriptor = {
     VORDemod::m_channelId,
     QStringLiteral("VOR Demodulator"),
-    QStringLiteral("6.3.3"),
+    QStringLiteral("6.10.0"),
     QStringLiteral("(c) Jon Beniston, M7RCE"),
     QStringLiteral("https://github.com/f4exb/sdrangel"),
     true,
