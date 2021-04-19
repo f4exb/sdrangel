6f4e596cca803764d6f8b17bc6a0b9f2bd1c67a5
f4exb <f4exb06@gmail.com>
2021-04-19 23:53:54 +0200

    Updated versions and changelogs

--- plugins/channelrx/demoddsd/dsddemodplugin.cpp
+++ plugins/channelrx/demoddsd/dsddemodplugin.cpp
@@ -30,7 +30,7 @@
 const PluginDescriptor DSDDemodPlugin::m_pluginDescriptor = {
     DSDDemod::m_channelId,
 	QStringLiteral("DSD Demodulator"),
-    QStringLiteral("6.7.0"),
+    QStringLiteral("6.10.1"),
 	QStringLiteral("(c) Edouard Griffiths, F4EXB"),
 	QStringLiteral("https://github.com/f4exb/sdrangel"),
 	true,
