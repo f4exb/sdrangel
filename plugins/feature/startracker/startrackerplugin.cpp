1e5c4343d197e8d90642d2ee92d91eda85b76b7e
f4exb <f4exb06@gmail.com>
2021-05-28 12:26:20 +0200

    Updated versions and changelogs

--- plugins/feature/startracker/startrackerplugin.cpp
+++ plugins/feature/startracker/startrackerplugin.cpp
@@ -30,7 +30,7 @@
 const PluginDescriptor StarTrackerPlugin::m_pluginDescriptor = {
     StarTracker::m_featureId,
     QStringLiteral("Star Tracker"),
-    QStringLiteral("6.10.3"),
+    QStringLiteral("6.13.0"),
     QStringLiteral("(c) Jon Beniston, M7RCE"),
     QStringLiteral("https://github.com/f4exb/sdrangel"),
     true,
