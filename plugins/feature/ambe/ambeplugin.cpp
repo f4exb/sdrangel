22e11f8cb4621a9e2a4bf0be273baf78c0f0f749
f4exb <f4exb06@gmail.com>
2022-05-26 05:13:46 +0200

    Updated versions and changelogs

--- plugins/feature/ambe/ambeplugin.cpp
+++ plugins/feature/ambe/ambeplugin.cpp
@@ -29,7 +29,7 @@
 const PluginDescriptor AMBEPlugin::m_pluginDescriptor = {
     AMBE::m_featureId,
 	QStringLiteral("AMBE Controller"),
-    QStringLiteral("7.2.0"),
+    QStringLiteral("7.2.1"),
 	QStringLiteral("(c) Edouard Griffiths, F4EXB"),
 	QStringLiteral("https://github.com/f4exb/sdrangel"),
 	true,
