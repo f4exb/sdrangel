ed754fd380b1ca7794da2cd614b0522bb0e97f63
f4exb <f4exb06@gmail.com>
2022-09-10 21:14:58 +0200

    Updated versions and changelogs

--- plugins/channeltx/modm17/m17modplugin.cpp
+++ plugins/channeltx/modm17/m17modplugin.cpp
@@ -28,7 +28,7 @@
 const PluginDescriptor M17ModPlugin::m_pluginDescriptor = {
     M17Mod::m_channelId,
     QStringLiteral("M17 Modulator"),
-    QStringLiteral("7.6.2"),
+    QStringLiteral("7.6.4"),
     QStringLiteral("(c) Edouard Griffiths, F4EXB"),
     QStringLiteral("https://github.com/f4exb/sdrangel"),
     true,
