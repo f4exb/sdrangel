c8b691c3dd22d5566658ebd08952985d3af531b8
f4exb <f4exb06@gmail.com>
2022-10-18 21:21:10 +0200

    Revert "Remove codec2 from Appveyor and Travis builds"
    This reverts commit 3d23d4023fadf1a77b21b21c6e3341411d5e432b.

---
+++ cmake/ci/build_codec2.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+sudo apt-get -y install libsamplerate0-dev libspeex-dev libspeexdsp-dev
+
+cd $HOME
+mkdir -p external && cd external
+mkdir -p drowe67 && cd drowe67
+
+git clone https://github.com/drowe67/codec2.git
+
+cd codec2
+git reset --hard "v1.0.3"
+mkdir -p build && cd build
+cmake ..
+sudo make install
+sudo ldconfig
