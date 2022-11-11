0d5404b6fb693257f863d734e3b05997d162e14d
Jon Beniston <jon@beniston.com>
2022-11-11 12:35:19 +0000

    Remove resize code from Map and ADS-B

--- plugins/channelrx/demodadsb/adsbdemodgui.cpp
+++ plugins/channelrx/demodadsb/adsbdemodgui.cpp
@@ -3783,18 +3783,7 @@ void ADSBDemodGUI::onWidgetRolled(QWidget* widget, bool rollDown)
     (void) widget;
     (void) rollDown;
 
-    RollupContents *rollupContents = getRollupContents();
-
-    if (rollupContents->hasExpandableWidgets()) {
-        setSizePolicy(sizePolicy().horizontalPolicy(), QSizePolicy::Expanding);
-    } else {
-        setSizePolicy(sizePolicy().horizontalPolicy(), QSizePolicy::Fixed);
-    }
-
-    int h = rollupContents->height() + getAdditionalHeight();
-    resize(width(), h);
-
-    rollupContents->saveState(m_rollupState);
+    getRollupContents()->saveState(m_rollupState);
     applySettings();
 }
 
