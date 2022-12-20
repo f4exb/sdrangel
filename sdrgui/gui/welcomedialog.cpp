653a4ea4f76f14439f3398029bcbcfd2f9df7390
Jon Beniston <jon@beniston.com>
2022-12-20 10:32:09 +0000

    Add welcome dialog for Android

---
+++ sdrgui/gui/welcomedialog.cpp
@@ -0,0 +1,31 @@
+///////////////////////////////////////////////////////////////////////////////////
+// Copyright (C) 2022 Jon Beniston, M7RCE                                        //
+//                                                                               //
+// This program is free software; you can redistribute it and/or modify          //
+// it under the terms of the GNU General Public License as published by          //
+// the Free Software Foundation as version 3 of the License, or                  //
+// (at your option) any later version.                                           //
+//                                                                               //
+// This program is distributed in the hope that it will be useful,               //
+// but WITHOUT ANY WARRANTY; without even the implied warranty of                //
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the                  //
+// GNU General Public License V3 for more details.                               //
+//                                                                               //
+// You should have received a copy of the GNU General Public License             //
+// along with this program. If not, see <http://www.gnu.org/licenses/>.          //
+///////////////////////////////////////////////////////////////////////////////////
+
+#include "gui/welcomedialog.h"
+#include "ui_welcomedialog.h"
+
+WelcomeDialog::WelcomeDialog(QWidget* parent) :
+	QDialog(parent),
+	ui(new Ui::WelcomeDialog)
+{
+	ui->setupUi(this);
+}
+
+WelcomeDialog::~WelcomeDialog()
+{
+	delete ui;
+}
