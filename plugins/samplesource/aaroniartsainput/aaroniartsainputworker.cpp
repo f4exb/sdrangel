5337cf3cf8582de305451a76250537120fdba537
Frank Werner <mail@hb9fxq.ch>
2023-04-19 11:42:54 +0200

    aaroniartsainput: Starting with build 12830 the stream header will contain the field 'sampleFrequency' for accurate bw calculation

--- plugins/samplesource/aaroniartsainput/aaroniartsainputworker.cpp
+++ plugins/samplesource/aaroniartsainput/aaroniartsainputworker.cpp
@@ -311,7 +311,7 @@ void AaroniaRTSAInputWorker::onReadyRead()
                     // qDebug() << jdoc.toJson();
                     quint64 endFreq = jdoc["endFrequency"].toDouble();
                     quint64 startFreq = jdoc["startFrequency"].toDouble();
-                    int bw = endFreq - startFreq;
+                    int bw = jdoc["sampleFrequency"].toInt();
                     quint64 midFreq = (endFreq + startFreq) / 2;
 
                     if ((bw != m_sampleRate) || (midFreq != m_centerFrequency))
