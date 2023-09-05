11042dc8898276b7466c4d4ab2dd8902538e33ad
Jon Beniston <jon@beniston.com>
2023-09-05 16:38:15 +0100

    Add command line options to start Remote TCP Sink on a specified device

--- sdrbase/mainparser.cpp
+++ sdrbase/mainparser.cpp
@@ -36,7 +36,13 @@ MainParser::MainParser() :
         "file",
         ""),
     m_scratchOption("scratch", "Start from scratch (no current config)."),
-    m_soapyOption("soapy", "Activate Soapy SDR support.")
+    m_soapyOption("soapy", "Activate Soapy SDR support."),
+    m_remoteTCPSinkOption("remote-tcp", "Start Remote TCP Sink"),
+    m_remoteTCPSinkAddressOption("remote-tcp-address", "Remote TCP Sink interface IP address (Default 127.0.0.1).", "address", "127.0.0.1"),
+    m_remoteTCPSinkPortOption("remote-tcp-port", "Remote TCP Sink port (Default 1234).", "port", "1234"),
+    m_remoteTCPSinkHWTypeOption("remote-tcp-hwtype", "Remote TCP Sink device hardware type (Optional. E.g. RTLSDR/SDRplayV3/AirspyHF).", "hwtype"),
+    m_remoteTCPSinkSerialOption("remote-tcp-serial", "Remote TCP Sink device serial (Optional).", "serial"),
+    m_listDevicesOption("list-devices", "List available physical devices.")
 {
 
     m_serverAddress = "";   // Bind to any address
@@ -44,6 +50,12 @@ MainParser::MainParser() :
     m_scratch = false;
     m_soapy = false;
     m_fftwfWindowFileName = "";
+    m_remoteTCPSink = false;
+    m_remoteTCPSinkAddress = "127.0.0.1";
+    m_remoteTCPSinkPort = 1234;
+    m_remoteTCPSinkHWType = "";
+    m_remoteTCPSinkSerial = "";
+    m_listDevices = false;
 
     m_parser.setApplicationDescription("Software Defined Radio application");
     m_parser.addHelpOption();
@@ -54,6 +66,12 @@ MainParser::MainParser() :
     m_parser.addOption(m_fftwfWisdomOption);
     m_parser.addOption(m_scratchOption);
     m_parser.addOption(m_soapyOption);
+    m_parser.addOption(m_remoteTCPSinkOption);
+    m_parser.addOption(m_remoteTCPSinkAddressOption);
+    m_parser.addOption(m_remoteTCPSinkPortOption);
+    m_parser.addOption(m_remoteTCPSinkHWTypeOption);
+    m_parser.addOption(m_remoteTCPSinkSerialOption);
+    m_parser.addOption(m_listDevicesOption);
 }
 
 MainParser::~MainParser()
@@ -65,19 +83,18 @@ void MainParser::parse(const QCoreApplication& app)
 
     int pos;
     bool ok;
+    QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
+    QRegularExpression ipRegex ("^" + ipRange
+                                 + "\\." + ipRange
+                                 + "\\." + ipRange
+                                 + "\\." + ipRange + "$");
+    QRegularExpressionValidator ipValidator(ipRegex);
 
     // server address
 
     QString serverAddress = m_parser.value(m_serverAddressOption);
     if (!serverAddress.isEmpty())
     {
-        QString ipRange = "(?:[0-1]?[0-9]?[0-9]|2[0-4][0-9]|25[0-5])";
-        QRegularExpression ipRegex ("^" + ipRange
-                                     + "\\." + ipRange
-                                     + "\\." + ipRange
-                                     + "\\." + ipRange + "$");
-        QRegularExpressionValidator ipValidator(ipRegex);
-
         if (ipValidator.validate(serverAddress, pos) == QValidator::Acceptable) {
             m_serverAddress = serverAddress;
         } else {
@@ -104,4 +121,37 @@ void MainParser::parse(const QCoreApplication& app)
 
     // Soapy SDR support
     m_soapy = m_parser.isSet(m_soapyOption);
+
+    // Remote TCP Sink options
+
+    m_remoteTCPSink = m_parser.isSet(m_remoteTCPSinkOption);
+
+    QString remoteTCPSinkAddress = m_parser.value(m_remoteTCPSinkAddressOption);
+    if (!remoteTCPSinkAddress.isEmpty())
+    {
+        if (ipValidator.validate(remoteTCPSinkAddress, pos) == QValidator::Acceptable) {
+            m_remoteTCPSinkAddress = remoteTCPSinkAddress;
+        } else {
+            qWarning() << "MainParser::parse: remote TCP Sink address invalid. Defaulting to " << m_remoteTCPSinkAddress;
+        }
+    }
+
+    QString remoteTCPSinkPortStr = m_parser.value(m_remoteTCPSinkPortOption);
+    int remoteTCPSinkPort = remoteTCPSinkPortStr.toInt(&ok);
+
+    if (ok && (remoteTCPSinkPort > 1023) && (remoteTCPSinkPort < 65536)) {
+        m_remoteTCPSinkPort = remoteTCPSinkPort;
+    } else {
+        qWarning() << "MainParser::parse: remote TCP Sink port invalid. Defaulting to " << m_serverPort;
+    }
+
+    m_remoteTCPSinkHWType = m_parser.value(m_remoteTCPSinkHWTypeOption);
+    m_remoteTCPSinkSerial = m_parser.value(m_remoteTCPSinkSerialOption);
+    m_listDevices = m_parser.isSet(m_listDevicesOption);
+
+    if (m_remoteTCPSink && m_remoteTCPSinkHWType.isEmpty() && m_remoteTCPSinkSerial.isEmpty())
+    {
+        qCritical() << "You must specify a device with either -remote-tcp-hwtype or -remote-tcp-serial";
+        exit (EXIT_FAILURE);
+    }
 }
