d2c1ba8e189d3c65ac46f1c684038e1df6a6a77b
Jon Beniston <jon@beniston.com>
2023-10-17 11:36:27 +0100

    Fix compilation on Windows

--- sdrbase/dsp/downchannelizer.cpp
+++ sdrbase/dsp/downchannelizer.cpp
@@ -16,6 +16,8 @@
 // along with this program. If not, see <http://www.gnu.org/licenses/>.          //
 ///////////////////////////////////////////////////////////////////////////////////
 
+#include <array>
+
 #include <QString>
 #include <QDebug>
 
