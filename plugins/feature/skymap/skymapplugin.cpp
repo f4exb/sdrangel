1d87e1e0515ece527e5e9b94afb3f926262e3040
f4exb <f4exb06@gmail.com>
2024-05-24 00:24:50 +0200

    Updated versions and changelogs

--- plugins/feature/skymap/skymapplugin.cpp
+++ plugins/feature/skymap/skymapplugin.cpp
@@ -34,7 +34,7 @@
 const PluginDescriptor SkyMapPlugin::m_pluginDescriptor = {
     SkyMap::m_featureId,
     QStringLiteral("Sky Map"),
-    QStringLiteral("7.20.0"),
+    QStringLiteral("7.21.0"),
     QStringLiteral("(c) Jon Beniston, M7RCE"),
     QStringLiteral("https://github.com/f4exb/sdrangel"),
     true,
