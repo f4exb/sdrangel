4252592045bcfeab8b5e2f35be0afc9867cdf1c7
Mykola Dvornik <mykola.dvornik@gmail.com>
2024-06-01 19:25:42 +0200

    Fix and refactor FindSoapySDR.cmake

--- cmake/Modules/FindSoapySDR.cmake
+++ cmake/Modules/FindSoapySDR.cmake
@@ -9,44 +9,45 @@ if(NOT SOAPYSDR_FOUND)
     set(${VERSION} "${CMAKE_MATCH_1}" PARENT_SCOPE)
   endfunction(_SOAPY_SDR_GET_ABI_VERSION)
 
-  pkg_search_module (LIBSOAPYSDR_PKG soapysdr>=0.4.0 SoapySDR>=0.4.0)
+  pkg_search_module (SOAPYSDR SoapySDR>=0.4.0)
 
-  if(NOT LIBSOAPYSDR_PKG_FOUND OR (DEFINED SOAPYSDR_DIR))
+  if(NOT SOAPYSDR_FOUND OR (DEFINED SOAPYSDR_DIR))
 
-    find_path (SOAPYSDR_INCLUDE_DIR
+    find_path (SOAPYSDR_INCLUDE_DIRS
       NAMES SoapySDR/Version.h
       HINTS ${SOAPYSDR_DIR}/include
-            ${LIBSOAPYSDR_PKG_INCLUDE_DIRS}
       PATHS /usr/include
             /usr/local/include
-      )
+    )
 
-    find_library (SOAPYSDR_LIBRARY
+    find_library (SOAPYSDR_LINK_LIBRARIES
       NAMES SoapySDR
       HINTS ${SOAPYSDR_DIR}/lib
             ${SOAPYSDR_DIR}/lib64
             ${CMAKE_INSTALL_PREFIX}/lib
             ${CMAKE_INSTALL_PREFIX}/lib64
-            ${LIBSOAPYSDR_PKG_LIBRARY_DIRS}
       PATHS /usr/local/lib
             /usr/local/lib64
             /usr/lib
             /usr/lib64
-      )
+    )
 
-    if (SOAPYSDR_INCLUDE_DIR AND SOAPYSDR_LIBRARY)
+    if (SOAPYSDR_INCLUDE_DIRS AND SOAPYSDR_LINK_LIBRARIES)
       set(SOAPYSDR_FOUND TRUE)
       # get the root of SoapySDR; used on cpack
-      string(REGEX REPLACE "/lib/.*${CMAKE_SHARED_LIBRARY_SUFFIX}" "" SOAPYSDR_ROOT ${SOAPYSDR_LIBRARY})
-      # get the soapy version; to using FindPkgConfig because we can use SOAPYSDR_DIR
-      _SOAPY_SDR_GET_ABI_VERSION(SOAPYSDR_ABI_VERSION ${SOAPYSDR_INCLUDE_DIR})
-      message (STATUS "Found SoapySDR: ${SOAPYSDR_INCLUDE_DIR}, ${SOAPYSDR_LIBRARY}")
-    else()
-      message (STATUS "SoapySDR not found")
+      string(REGEX REPLACE "/lib/.*${CMAKE_SHARED_LIBRARY_SUFFIX}" "" SOAPYSDR_LIBDIR ${SOAPYSDR_LINK_LIBRARIES})
+      # get the soapy version
+      _SOAPY_SDR_GET_ABI_VERSION(SOAPYSDR_VERSION ${SOAPYSDR_INCLUDE_DIRS})
     endif()
 
-    mark_as_advanced(SOAPYSDR_INCLUDE_DIR SOAPYSDR_LIBRARY SOAPYSDR_ROOT SOAPYSDR_ABI_VERSION)
+  endif(NOT SOAPYSDR_FOUND OR (DEFINED SOAPYSDR_DIR))
 
-  endif(NOT LIBSOAPYSDR_PKG_FOUND OR (DEFINED SOAPYSDR_DIR))
+  if (SOAPYSDR_FOUND)
+    message (STATUS "Found SoapySDR: version ${SOAPYSDR_VERSION}, ${SOAPYSDR_LIBDIR}, ${SOAPYSDR_INCLUDE_DIRS}, ${SOAPYSDR_LINK_LIBRARIES}")
+  else()
+    message (STATUS "SoapySDR not found")
+  endif()
+
+  mark_as_advanced(SOAPYSDR_INCLUDE_DIRS SOAPYSDR_LINK_LIBRARIES SOAPYSDR_LIBDIR SOAPYSDR_VERSION)
 
 endif(NOT SOAPYSDR_FOUND)
