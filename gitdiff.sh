0cc853f1772d939ad664c9563cdafb342f7505c7
f4exb <f4exb06@gmail.com>
2024-08-13 22:27:55 +0200

    Updated changelogs

--- gitdiff.sh
+++ gitdiff.sh
@@ -1,2 +1,9 @@
 #!/bin/sh
-git diff --name-only ${1}..${2} | grep plugins/ | cut -d'/' -f2,3 | sort -u
+BASEDIR=$(dirname $0)
+PLUGINS=$(git diff --name-only ${1}..${2} | grep plugins/ | cut -d'/' -f2,3 | sort -u)
+for plugin in $PLUGINS
+do
+    FILE=$(find $BASEDIR/plugins/$plugin -name "*plugin.cpp")
+    sed -i -E "s/QStringLiteral\(\"7\.(.*)\"\)/QStringLiteral\(\"7\.22\.0\"\)/" $FILE
+done
+
