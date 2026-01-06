bbf9fb2b8f565c93d6ecc512988680fd98972a04
Jon Beniston <jon@beniston.com>
2026-01-06 14:11:59 +0000

    Call FIND_LIBRARY per library.

--- cmake/Modules/FindLibInmarsatC.cmake
+++ cmake/Modules/FindLibInmarsatC.cmake
@@ -7,9 +7,11 @@ IF (NOT INMARSATC_FOUND)
               /usr/local/include
     )
 
+    # We don't currently use inmarsatc_demodulator
+
     FIND_LIBRARY(
-        INMARSATC_LIBRARIES
-        NAMES inmarsatc_decoder  inmarsatc_parser
+        INMARSATC_DECODER_LIBRARY
+        NAMES inmarsatc_decoder
         HINTS ${INMARSATC_DIR}/lib
               ${INMARSATC_DIR}/lib64
         PATHS /usr/lib
@@ -18,13 +20,25 @@ IF (NOT INMARSATC_FOUND)
               /usr/local/lib64
     )
 
-    if (INMARSATC_INCLUDE_DIR AND INMARSATC_LIBRARIES)
+    FIND_LIBRARY(
+        INMARSATC_PARSER_LIBRARY
+        NAMES inmarsatc_parser
+        HINTS ${INMARSATC_DIR}/lib
+              ${INMARSATC_DIR}/lib64
+        PATHS /usr/lib
+              /usr/lib64
+              /usr/local/lib
+              /usr/local/lib64
+    )
+
+    if (INMARSATC_INCLUDE_DIR AND INMARSATC_DECODER_LIBRARY AND INMARSATC_PARSER_LIBRARY)
+        set(INMARSATC_LIBRARIES ${INMARSATC_DECODER_LIBRARY} ${INMARSATC_PARSER_LIBRARY} CACHE INTERNAL "inmarsatc libraries")
         set(INMARSATC_FOUND TRUE CACHE INTERNAL "inmarsatc found")
         message(STATUS "Found inmarsatc: ${INMARSATC_INCLUDE_DIR}, ${INMARSATC_LIBRARIES}")
-    else (INMARSATC_INCLUDE_DIR AND INMARSATC_LIBRARIES)
+    else ()
         set(INMARSATC_FOUND FALSE CACHE INTERNAL "inmarsatc found")
         message(STATUS "inmarsatc not found.")
-    endif (INMARSATC_INCLUDE_DIR AND INMARSATC_LIBRARIES)
+    endif ()
 
     mark_as_advanced(INMARSATC_INCLUDE_DIR INMARSATC_LIBRARIES)
 
