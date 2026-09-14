52e6f52bd4c96c525a0ab0e756db173a6a21e793
Robin Getz <rgetz503@gmail.com>
2026-09-14 19:16:45 -0400

    mypositiondialog: Add mapquest button to dialog
    Add a world icon button to the position dialog that opens
    mapquest.com in the default browser. This makes users aware that
    latitude, longitude can be obtained from standard web sites when
    a GPS or other positioning device is not available, without
    requiring them to search for a way to obtain the information.
    
    This provides an (in)direct path to the values needed by SDRangel.
    
    Signed-off-by: Robin Getz <rgetz503@gmail.com>

--- sdrgui/gui/mypositiondialog.cpp
+++ sdrgui/gui/mypositiondialog.cpp
@@ -23,6 +23,7 @@
 #include "ui_myposdialog.h"
 #include "maincore.h"
 
+#include <QDesktopServices>
 #include <QGeoCoordinate>
 #include <QJsonArray>
 #include <QJsonDocument>
@@ -30,6 +31,7 @@
 #include <QNetworkAccessManager>
 #include <QNetworkReply>
 #include <QNetworkRequest>
+#include <QUrl>
 
 MyPositionDialog::MyPositionDialog(MainSettings& mainSettings, QWidget* parent) :
     QDialog(parent),
@@ -42,6 +44,11 @@ MyPositionDialog::MyPositionDialog(MainSettings& mainSettings, QWidget* parent) 
     ui->longitudeSpinBox->setValue(m_mainSettings.getLongitude());
     ui->altitudeSpinBox->setValue(m_mainSettings.getAltitude());
     ui->autoUpdatePosition->setChecked(m_mainSettings.getAutoUpdatePosition());
+
+    connect(ui->mapquest, &QToolButton::clicked, this, [] {
+        QDesktopServices::openUrl(QUrl("https://developer.mapquest.com/documentation/tools/latitude-longitude-finder/"));
+    });
+
 }
 
 MyPositionDialog::~MyPositionDialog()
